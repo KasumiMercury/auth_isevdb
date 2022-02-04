7668fb36049ae92cd9eb7c1ecf57739b182947e1
Mercury <naoki_shira@outlook.jp>
2022-02-04 18:00:30 +0900

    set lint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        browser: true,
+        es2021: true,
+    },
+    extends: ["eslint:recommended", "plugin:vue/essential", "prettier"],
+    parserOptions: {
+        ecmaVersion: "latest",
+    },
+    plugins: ["vue"],
+    rules: {
+        "vue/prop-name-casing": "off",
+        indent: ["error", 4],
+        "linebreak-style": ["error", "windows"],
+        quotes: ["error", "double"],
+        semi: ["error", "never"],
+    },
+}
